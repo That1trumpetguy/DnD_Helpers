77daa071688e91e84fde450aa9524ceacc552ce4
That1trumpetguy <104122775+That1trumpetguy@users.noreply.github.com>
2024-09-21 14:25:46 -0500

    Created Image_Reader.py
    Created the actual python for the image reader file, I still need to delete the original text only one

---
+++ Magic_Item_Maker/Image_Reader.py
@@ -0,0 +1,24 @@
+#This is an attempt to use unstructured as a way to read informatiton off of images for magic item stat-blocks
+
+"""
+TO-DO: 
+pip install all relevant packadges
+-unstructured[all-docs]
+-libmagic-dev
+-poppler-utils
+-tesseract-ocr
+-libreoffice
+-pandoc
+
+From there I need to make sure that my unstructured
+is working using the following code block
+"""
+#%%
+from unstructured.partition.auto import partition
+
+elements = partition(filename="example-docs/eml/fake-email.eml")
+print("\n\n".join([str(el) for el in elements]))
+#%%
+
+#import block
+import numpy as np
